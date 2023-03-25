ca76ba9b6c3ecadcdd34853d79fe0379ee423408
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2023-03-25 17:15:04 -0600

    working on navbar styling wth style objects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,15 @@ import { NavLink } from 'react-router-dom'
 // import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+    const myStyle = {
+        color: "white",
+        backgroundColor: "DodgerBlue",
+        padding: "10px",
+        fontFamily: "Sans-Serif"
+      };
     // const state = useSelector(state => state.handleCart)
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
+        <nav style={myStyle}>
             <div className="container">
                 <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"> React Ecommerce</NavLink>
                 <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
