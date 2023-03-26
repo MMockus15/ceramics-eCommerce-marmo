8ee3e7edb06b1240fd723b61e8576e429749cf43
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2023-03-26 17:16:56 -0600

    setting up root index js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { configureStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
 
+import productsReducer from "./redux/reducer/productsReducer";
+import cartReducer from "./redux/reducer/cartReducer";
+import { Home, Products, Product, AboutPage, Cart, Checkout, Login, Register, PageNotFound } from "./pages";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import store from './redux/store';
-// import Products from './components/Product/Products';
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer
+});
 
+const store = configureStore({
+  reducer: rootReducer
+});
 
-import { Home } from "./pages"
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <Routes>
-        <Route path ="/" element={<Home />} />
-        {/* <Route path ="/product" element={<Products />} />
-        <Route path ="/" element={<Home />} />
-        <Route path ="/" element={<Home />} />
-        <Route path ="/" element={<Home />} />
-        <Route path ="/" element={<Home />} />
-        <Route path ="/" element={<Home />} />
-        <Route path ="/" element={<Home />} />
-        <Route path ="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<Product />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
-      {/* </Provider> */}
+    </Provider>
   </BrowserRouter>
 );
