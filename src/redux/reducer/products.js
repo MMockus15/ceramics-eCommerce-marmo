f31ad5f9f584f65bd6329420f90615e5c2f74f86
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2023-04-02 18:13:43 -0600

    images need changing

--- src/redux/reducer/products.js
+++ src/redux/reducer/products.js
@@ -3,7 +3,7 @@ const products = [
 	  id: 1,
 	  name: "Hand-thrown Ceramic Mug",
 	  description: "This beautiful ceramic mug is hand-thrown by skilled artisans and features unique patterns and colors.",
-	  image: "https://example.com/hand-thrown-ceramic-mug.jpg",
+	  image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.etsystatic.com%2F8740835%2Fr%2Fil%2F221f3d%2F1409835265%2Fil_500x500.1409835265_erg5.jpg&tbnid=STuztXqBiXwIcM&vet=12ahUKEwiCneb5sYz-AhUyoVsKHYCHBLMQMygEegUIARD_AQ..i&imgrefurl=https%3A%2F%2Fwww.etsy.com%2Flisting%2F519688604%2Fhandthrown-pottery-mug&docid=PWCN-2ueR1GdVM&w=500&h=500&q=handthrown%20pottery&ved=2ahUKEwiCneb5sYz-AhUyoVsKHYCHBLMQMygEegUIARD_AQ",
 	  price: 25.99,
 	  category: "Mugs",
 	  quantity: 10
@@ -12,7 +12,7 @@ const products = [
 	  id: 2,
 	  name: "Hand-thrown Ceramic Vase",
 	  description: "This stunning ceramic vase is hand-thrown by skilled artisans and features a beautiful shape and texture.",
-	  image: "https://example.com/hand-thrown-ceramic-vase.jpg",
+	  image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.etsystatic.com%2F9244241%2Fr%2Fil%2Fb5182a%2F1881007822%2Fil_570xN.1881007822_8lm7.jpg&tbnid=hFj_6pJOc4GbfM&vet=12ahUKEwiCneb5sYz-AhUyoVsKHYCHBLMQ94IIKBJ6BQgBEKMC..i&imgrefurl=https%3A%2F%2Fwww.etsy.com%2Flisting%2F693107924%2Fhand-thrown-ceramic-table-setting-made&docid=Mt27G0W4IS3sUM&w=570&h=428&q=handthrown%20pottery&ved=2ahUKEwiCneb5sYz-AhUyoVsKHYCHBLMQ94IIKBJ6BQgBEKMC",
 	  price: 59.99,
 	  category: "Vases",
 	  quantity: 5
