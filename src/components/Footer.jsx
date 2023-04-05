bc6ce3e320c4a6671118b4b570c994f08706efee
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2023-04-05 15:22:15 -0600

    main page styleing still needs lots of css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,54 @@
 import React from "react";
 
+
 const Footer = () => {
   return (
-    <>
-      <footer className="mb-0 text-center">
-        <div className="d-flex align-items-center justify-content-center pb-5"
-        style={{backgroundColor: "tan"}}>
-          <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://mmockus15.github.io/mmockus-professional-portfolio/" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">MarMo</a>
-            </p>
-            <a className="text-dark fs-4" href="https://github.com/MMockus15" target="_blank" rel="noreferrer">
-              <i className="fa fa-github"></i>
-            </a>
+    <footer className="bg-dark text-light pt-5 pb-3">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 text-center mb-4 mb-md-0">
+            <h4>Follow Us</h4>
+            <div className="d-flex justify-content-center">
+              <a href="https://www.facebook.com/marla.mockus" className="me-3 text-decoration-none">
+                <i className="fab fa-facebook fa-2x"></i>
+              </a>
+              <a href="https://twitter.com/MarlaMockus" className="me-3 text-decoration-none">
+                <i className="fab fa-twitter fa-2x"></i>
+              </a>
+              <a href="https://www.instagram.com/_marmo/" className="me-3 text-decoration-none">
+                <i className="fab fa-instagram fa-2x"></i>
+              </a>
+            </div>
+          </div>
+          <div className="col-md-4 text-center mb-4 mb-md-0">
+            <h4>Contact Us</h4>
+            <p>1234 Main Street, Anytown USA</p>
+            <p>Phone: 555-555-5555</p>
+            <p>Email: info@example.com</p>
+          </div>
+          <div className="col-md-4 text-center">
+            <h4>About Us</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod elit sed ligula pretium, a ultrices magna sagittis. Integer ultricies, mi in bibendum dapibus, lacus dui tristique ex, vitae eleifend felis velit id ante.</p>
           </div>
         </div>
-      </footer>
-    </>
+        <hr />
+        <div className="row">
+          <div className="col-12 text-center">
+            <p>Made with ❤️ by <a href="https://mmockus15.github.io/mmockus-professional-portfolio/" className="text-light text-decoration-underline" target="_blank" rel="noreferrer">MarMo</a></p>
+          </div>
+        </div>
+      </div>
+      <style>
+        {`
+        @media (max-width: 576px) {
+          .footer-links {
+            display: flex;
+            flex-direction: column;
+          }
+        }
+        `}
+      </style>
+    </footer>
   );
 };
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,63 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
+
+const ProductContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+  margin: 20px 0;
+
+  h2 {
+    text-align: center;
+    margin-bottom: 20px;
+    font-size: 2rem;
+    color: #332e2c;
+    font-family: "Indie Flower", cursive;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  h3 {
+    margin: 10px 0;
+    color: #332e2c;
+    font-size: 1.5rem;
+    font-family: "Indie Flower", cursive;
+  }
+
+  p {
+    color: #332e2c;
+    font-size: 1.2rem;
+    margin-bottom: 5px;
+    font-family: "Indie Flower", cursive;
+  }
+
+  button {
+    background-color: #332e2c;
+    color: #e1c8ab;
+    border: none;
+    border-radius: 25px;
+    padding: 10px 20px;
+    font-weight: bold;
+    text-transform: uppercase;
+    text-decoration: none;
+    margin: 10px 0;
+    transition: all 0.3s ease;
+
+    &:hover {
+      background-color: #e1c8ab;
+      color: #332e2c;
+    }
+  }
+`;
 
 const ProductList = () => {
   const products = useSelector(state => state.products.products);
 
   return (
-    <div>
+    <ProductContainer>
       <h2>Hand-thrown Ceramics</h2>
       {products.map(product => (
         <div key={product.id}>
@@ -16,8 +68,9 @@ const ProductList = () => {
           <button>Add to Cart</button>
         </div>
       ))}
-    </div>
+    </ProductContainer>
   );
 };
 
 export default ProductList;
+
