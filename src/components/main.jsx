a31ae198a128ecce623724eeaadf2c065d03ba33
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2023-04-05 15:14:32 -0600

    added styling to main

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,29 +1,62 @@
 import React from "react";
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+  background-color: #f4ede8;
+  padding: 50px;
+`;
+
+const Card = styled.div`
+  background-color: #332e2c;
+  color: #e1c8ab;
+  border: none;
+  border-radius: 10px;
+  overflow: hidden;
+  margin: 0 20px;
+`;
+
+const CardImg = styled.img`
+  height: 500px;
+  object-fit: cover;
+`;
+
+const CardTitle = styled.h5`
+  font-size: 3rem;
+  font-weight: 300;
+  text-align: center;
+  margin-top: 50px;
+`;
+
+const CardText = styled.p`
+  font-size: 1.5rem;
+  font-weight: 300;
+  text-align: center;
+  margin: 20px 0;
+`;
 
 const Home = () => {
   return (
-    <>
+    <HomeContainer>
       <div className="hero border-1 pb-3">
-        <div className="card bg-dark text-white border-0 mx-3">
-          <img
+        <Card className="card">
+          <CardImg
             className="card-img img-fluid"
             src="./assets/main.png.jpg"
             alt="Card"
-            height={500}
           />
           <div className="card-img-overlay d-flex align-items-center">
             <div className="container">
-              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
-              <p className="card-text fs-5 d-none d-sm-block ">
+              <CardTitle>New Season Arrivals</CardTitle>
+              <CardText>
                 This is a wider card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
-              </p>
+              </CardText>
             </div>
           </div>
-        </div>
+        </Card>
       </div>
-    </>
+    </HomeContainer>
   );
 };
 
