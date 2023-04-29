6e3da264dee0514bd024521e74d5d207c7f0a4b1
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2023-04-28 19:01:41 -0600

    adding userReducer to get login functional

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import { combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import cartReducer from './reducers/cartReducer';
 import productsReducer from './reducers/productsReducer';
+import userReducer from './reducer/userReducer';
 
 const rootReducer = combineReducers({
   cart: cartReducer,
   products: productsReducer,
+  user: userReducer,
 });
 
 const store = configureStore(rootReducer, applyMiddleware(thunk));
